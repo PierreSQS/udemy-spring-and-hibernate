8649b6e5d3799d4e2796814534b9898dfc1eabd9
PierreSQS <nomo_messina@web.de>
2021-08-23 20:31:33 +0200

    Section60[Chap474..476]: extended Controller and added dependency for new Annotation

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -2,9 +2,11 @@ package com.luv2code.springdemo.rest;
 
 import com.luv2code.springdemo.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,12 +14,36 @@ import java.util.List;
 @RequestMapping("api")
 public class StudentRestController {
 
-    @GetMapping("students")
-    public List<Student> listStudents() {
-        List<Student> students = new ArrayList<>();
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadStudentList() {
+        students = new ArrayList<>();
         students.add(new Student("Pierrot", "Mongonnam"));
         students.add(new Student("Elke", "Borgmann"));
         students.add(new Student("Odile", "Baima"));
+    }
+
+    @GetMapping("students")
+    public List<Student> listStudents() {
         return students;
+    }
+
+    @GetMapping("students/{studentId}")
+    public Student getStudentByID(@PathVariable Integer studentId) {
+
+        Student foundStudent = null;
+
+        if (studentId > students.size()) {
+            foundStudent = new Student("Student","Not found");
+        } else {
+            for (Student student:students) {
+                if(student.getFirstName().equals(students.get(studentId).getFirstName())){
+                    foundStudent = student;
+                }
+            }
+        }
+        return foundStudent;
+
     }
 }
