ea4b02b5dc58536253f2a67b6d9c53798545a8a2
PierreSQS <nomo_messina@web.de>
2021-08-25 15:01:23 +0200

    Section64[Chap496..497]-Get Single customer: extended Controller

--- spring-json-rest-demo/src/main/java/com/luv2code/springdemo/controller/CustomerController.java
+++ spring-json-rest-demo/src/main/java/com/luv2code/springdemo/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.controller;
 import com.luv2code.springdemo.entity.Customer;
 import com.luv2code.springdemo.service.CustomerService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class CustomerController {
     @GetMapping("customers")
     public List<Customer> getCustomers() {
         return customerService.getCustomers();
+    }
+
+    @GetMapping("customers/{customerID}")
+    public Customer getCustomerByID(@PathVariable int customerID) {
+        return customerService.getCustomer(customerID);
     }
 }
